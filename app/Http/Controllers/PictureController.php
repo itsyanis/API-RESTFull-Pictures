913e7bec7a61441cbc0f8a31bd746f4cba0622d7
itsyanis <yanis.maafi@viacesi.fr>
2022-08-31 19:07:25 +0200

    feat(Picture_API) : API Rest

---
+++ app/Http/Controllers/PictureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PictureRequest;
+use App\Models\Picture;
+use Illuminate\Http\Request;
+
+class PictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pictures = Picture::all();
+        return response()->json($pictures);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PictureRequest $request)
+    {
+        $picture = Picture::create([
+                        'title' => $request->title,
+                        'description' => $request->description,
+                        'image' => $request->image,
+                    ]);
+
+        return response()->json($picture);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Picture $picture)
+    {
+        $picture = Picture::findOrFail($picture->id);
+        return response()->json($picture);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PictureRequest $request, Picture $picture)
+    {
+        $picture = Picture::findOrFail($picture->id);
+        $picture->title = $request->title;
+        $picture->description = $request->description;
+        $picture->image = $request->image;
+        $picture->save();
+
+        return response()->json($picture);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $picture = Picture::findOrFail($id);
+        
+        if($picture->delete()){
+            return response()->json("Picture deleted successfully");
+        }
+    }
+}
